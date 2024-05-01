890c919e73b09fba047700f138188a7e8c2cbd48
SantiSB <a.santiago.salas.b@gmail.com>
2024-05-01 00:44:52 -0500

    forecast component

--- src/app/forecast/page.tsx
+++ src/app/forecast/page.tsx
@@ -1,55 +1,19 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-import styles from "./page.module.scss";
+import React, { Suspense } from "react";
+import dynamic from "next/dynamic";
 import Loading from "@/components/Loading";
-import useWeatherData from "@/hooks/useWeather";
-import useLocationFetch from "@/hooks/useLocation";
-import Error from "@/components/Error";
-import CurrentDay from "@/components/CurrentDay";
-import DayForecast from "@/components/DayForecast";
+
+const ForecastWithNoSSR = dynamic(
+  () => import("../../components/ForecastComponent"),
+  {
+    ssr: false,
+  }
+);
 
 const Forecast: React.FC = () => {
-  const searchParams = useSearchParams();
-  const city = searchParams.get("city");
-  const zip = searchParams.get("zip");
-  const country = searchParams.get("country");
-
-  const { location, loading, error } = useLocationFetch({ city, zip, country });
-  const { weather, currentDayForecast, fiveDayForecast, hourlyTemperatures } =
-    useWeatherData(location);
-
-  if (
-    loading ||
-    !location ||
-    !weather ||
-    !currentDayForecast ||
-    fiveDayForecast.length === 0
-  ) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <Error message={error.message} />;
-  }
-
   return (
-    <div className={styles.container}>
-      <h1>Weather in {location.name}</h1>
-      <h3>
-        Today: {new Date(currentDayForecast.dt * 1000).toLocaleDateString()}
-      </h3>
-      <div className={styles.currentDayContainer}>
-        <CurrentDay
-          currentDayForecast={currentDayForecast}
-          hourlyTemperatures={hourlyTemperatures}
-        />
-      </div>
-      <div className={styles.fiveDayContainer}>
-        {fiveDayForecast.map((day, index) => (
-          <DayForecast key={index} day={day} index={index} />
-        ))}
-      </div>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <ForecastWithNoSSR />
+    </Suspense>
   );
 };
 
